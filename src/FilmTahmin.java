3e2ec4e3e4069c1690f556be329470e6ce25ce94
GulsenGenc <gulsengenctuncer@gmail.com>
2022-03-27 00:12:44 +0300

    27.03.2022

---
+++ src/FilmTahmin.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class FilmTahmin {
+
+    public static void main(String[] args) {
+
+
+         List<String> film = new ArrayList<>(Arrays.asList("JOKER","INCEPTION","PIYANIST","GREENMILE","LEON","GODFATHER","JURASSICPARK","TITANIC"));
+/*
+ TASK :
+  yukarıdaki film listinde bulunan filmlerden herhangi birisini kullanıcının filim ismini  görmeden
+  filmin index nosuna göre sectiriniz
+  seçilen filmin  ismini filmin harf saysının 2 katı kadar hak tanıyarak tahmin etmesini
+  sağlayan method create ediniz...
+  Ahanda TRICK...
+  kullanıcının sectiği filmin harf sayısını  console yazdırınız.
+  kullanıcının sectiği film için tahmin hakkını  console yazdırınız.
+  kullanıcının her tahmininde kalan hak sayısını console yazdırınız.
+  kullanıcının her tahmininde doğru ve yanlış tahmin  sayısını console yazdırınız.
+  kullanıcının kaybedip veya kazandığını ve tahmin etmesi gereken filmin ismini  console yazdırınız.
+ */
+
+
+
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("~~~FILM TAHMIN OYUNUNA HOSGELDINIZ~~~");
+
+            System.out.println("1-"+(film.size())+" arasinda bir sayi giriniz");
+
+        }
+    }
+
---
+++ src/SwitchCase/Soru_Bankamatik.java
@@ -0,0 +1,11 @@
+package SwitchCase;
+
+public class Soru_Bankamatik {
+    public static void main(String[] args) {
+        /*
+    Bakıye (başlangıç bakıyesi 1000 tl) öğrenme, para yatırma ve cekme,
+     çıkış işlemlerinin yapıldıgı bir ATM app. code create ediniz
+     */
+
+    }
+}
