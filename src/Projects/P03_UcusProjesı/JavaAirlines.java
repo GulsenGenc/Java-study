d398bf56141993f3238a35e40db0aaafca579f38
GulsenGenc <gulsengenctuncer@gmail.com>
2022-04-01 07:21:39 +0300

    01.04.20022

--- src/Projects/P03_UcusProjesı/JavaAirlines.java
+++ src/Projects/P03_UcusProjesı/JavaAirlines.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class JavaAirlines {
     public static void main(String[] args) {
-        Yolcu ayhan=new Yolcu("B","tek",33);
+
 
 
 
---
+++ src/Projects/P03_UcusProjesı/Runner.java
@@ -0,0 +1,19 @@
+package Projects.P03_UcusProjesı;
+
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String[] args) {
+
+
+        Yolcu yolcu1=new Yolcu("Gülşen","Genç",28,"C",1,30);
+
+        yolcu1.biletSatınAl();
+
+
+
+
+
+
+    }
+}
