fa8ed8a19becefb95f6d1b94b754dce73ee3fcf2
GulsenGenc <gulsengenctuncer@gmail.com>
2022-04-21 14:36:04 +0300

    21.04.2022

--- src/_17_Set/Ex01.java
+++ src/_17_Set/Ex01.java
@@ -1,5 +1,7 @@
 package _17_Set;
 
+import java.util.HashSet;
+
 public class Ex01 {
     public static void main(String[] args) {
         /* TASK:
@@ -8,9 +10,21 @@ public class Ex01 {
     yine set icindekileri yazdiracak ayrica bir metod daha create edip print ediniz.
 
      */
-
+        HashSet<Integer> hs = new HashSet<>();
+        int arr[] = new int[]{1, 2, 3, 4, 5, 6, 7};
+        elementEkle(hs,arr);
 
     }
-publi
 
+    private static void elementEkle(HashSet<Integer> hs, int[] arr) {
+        for (Integer sayi:arr) {
+            hs.add(sayi);
+        }
+        yazdir(hs);
+    }
+    public  static  void  yazdir(HashSet<Integer> hs){
+        for (Integer s:hs) {
+            System.out.print(s+" ");
+        }
+    }
 }
